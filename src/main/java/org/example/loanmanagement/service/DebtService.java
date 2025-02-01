6bc43f8ba016e04c7cf0c700c263f0d2f117ff23
sirojiddinzulxaydarov <sirojiddinzulxaydarov@gmail.com>
2025-02-01 16:35:28 +0500

    DebtService

---
+++ src/main/java/org/example/loanmanagement/service/DebtService.java
@@ -0,0 +1,40 @@
+package org.example.loanmanagement.service;
+
+import org.example.loanmanagement.entity.Debt;
+import org.example.loanmanagement.repository.DebtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DebtService {
+    @Autowired
+    private DebtRepository debtRepository;
+
+    public List<Debt> findAll() {
+        return debtRepository.findAll();
+    }
+
+    public List<Debt> findByCustomer(Integer id){
+        return debtRepository.findByCustomerId(id);
+    }
+
+    public Debt createDebt(Debt debt){
+        return debtRepository.save(debt);
+    }
+
+    public Debt updateDebt(Integer id, Debt debtDetails){
+        Debt debt = debtRepository.findById(id).get();
+        debt.setAmount(debtDetails.getAmount());
+        debt.setDate(debtDetails.getDate());
+        debt.setDescription(debtDetails.getDescription());
+        return debtRepository.save(debt);
+
+    }
+
+    public void deleteDebt(Integer id){
+        debtRepository.deleteById(id);
+    }
+}
