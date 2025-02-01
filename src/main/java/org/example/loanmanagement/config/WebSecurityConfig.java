02370d46a62266864434e9243f2267f105afc7a6
sirojiddinzulxaydarov <sirojiddinzulxaydarov@gmail.com>
2025-02-02 02:21:56 +0500

    permit for all requests

--- src/main/java/org/example/loanmanagement/config/WebSecurityConfig.java
+++ src/main/java/org/example/loanmanagement/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package org.example.loanmanagement.config;
 
-import org.example.loanmanagement.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Autowired
-    private UserService userService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
+        return http.build();
+    }
 }
